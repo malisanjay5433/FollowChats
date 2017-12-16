17fcc4e2058327f19411dd8cd933471d8a91c550
Sanjay Mali <sanjaymali@sanjays-macbook-pro.local>
2017-12-16 19:43:25 +0530

    Initial Commit

---
+++ FollowChats/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FollowChats
+//
+//  Created by Sanjay Mali on 16/12/17.
+//  Copyright © 2017 Sanjay Mali. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
